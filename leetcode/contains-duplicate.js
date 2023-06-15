80d6e8df56c52fe20d92388b1271b6fdeec6a9c3
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-15 13:51:25 +0300

    Solve the challenge initially

---
+++ leetcode/contains-duplicate.js
@@ -0,0 +1,33 @@
+/*
+217. Contains Duplicate
+
+Given an integer array nums, return true if any value appears at least twice in the 
+array, and return false if every element is distinct.
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Input: nums = [1,2,3,4]
+Output: false
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+### CONSTRAINTS
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+*/
+
+const containsDuplicate = array => {
+  const uniqueValues = new Set();
+  for (let item of array) {
+    if (uniqueValues.has(item)) return true;
+    uniqueValues.add(item);
+  }
+  return false;
+};
+
+const nums = [1, 2, 3, 4];
+
+console.log(containsDuplicate(nums));

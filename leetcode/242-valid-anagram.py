dcb964c5782f40df0d0fd68a68b02d24a4b8ff4e
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-22 13:59:07 +0300

    Solve in less space, more time

--- leetcode/242-valid-anagram.py
+++ leetcode/242-valid-anagram.py
@@ -36,7 +36,23 @@ def is_anagram(s, t):
     return False
 
 
-s = "anagram"
+def is_anagram_less_space(s, t):
+    if len(s) != len(t):
+        return False
+
+    az_tracker = [0] * 26
+    for i in range(len(s)):
+        az_tracker[ord(s[i]) - ord("a")] += 1
+        az_tracker[ord(t[i]) - ord("a")] -= 1
+
+    for item in az_tracker:
+        if item != 0:
+            return False
+
+    return True
+
+
+s = "anagrat"
 t = "nagaram"
 
-print(is_anagram(s, t))
+print(is_anagram_less_space(s, t))

2e975984d67cdbe7e0205109104b416e1fdb432a
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-23 15:07:37 +0300

    Tidy up presentation

--- leetcode/217-contains-duplicate.py
+++ leetcode/217-contains-duplicate.py
@@ -1,25 +1,5 @@
-""" 
-Given an integer array nums, return true if any value appears at least twice in the 
-array, and return false if every element is distinct.
-
-Input: nums = [1,2,3,1]
-Output: true
-
-Input: nums = [1,2,3,4]
-Output: false
-
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
-
-### CONSTRAINTS
-1 <= nums.length <= 105
--109 <= nums[i] <= 109
-
-"""
-
-
 class Solution:
-    def containsDuplicate(nums) -> bool:
+    def containsDuplicate(self, nums) -> bool:
         reg_unique = set([])
         for i in nums:
             if i in reg_unique:
@@ -28,7 +8,8 @@ class Solution:
         return False
 
 
+solution = Solution()
 nums = [1, 2, 3, 1]
 nums2 = [1, 2, 3, 4]
 nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
-print(Solution.containsDuplicate(nums2))
+print(solution.containsDuplicate(nums3))

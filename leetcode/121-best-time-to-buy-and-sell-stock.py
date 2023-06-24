11dac65629ff5b26a139b0a194937bee0bb2c979
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-24 15:02:55 +0300

    Solved 121, but add to REVISIT.md

---
+++ leetcode/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0  # Buy
+        right = 1  # Sell
+        max_profit = 0
+        while right < len(prices):
+            current_profit = prices[right] - prices[left]  # our current Profit
+            if prices[left] < prices[right]:
+                max_profit = max(current_profit, max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
---
+++ leetcode/REVISIT.md
@@ -0,0 +1,3 @@
+Problems to revisit:
+
+- 121 - Best time to buy and sell stock (double pointer)

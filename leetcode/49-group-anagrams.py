4bd90a864d1ddc1bc55a0324b284878ab460b7f1
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-26 16:14:49 +0300

    Solve 2nd time with possibly better time complexity

--- leetcode/49-group-anagrams.py
+++ leetcode/49-group-anagrams.py
@@ -11,3 +11,17 @@ class Solution:
             else:
                 grouped[sort].append(str)
         return grouped.values()
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grouped = {}
+        for str in strs:
+            counted = [0] * 26
+            for c in str:
+                counted[ord(c) - ord("a")] += 1
+            if grouped.get(tuple(counted)) is None:
+                grouped[tuple(counted)] = [str]
+            else:
+                grouped[tuple(counted)].append(str)
+        return grouped.values()

55ef4b812962cdc703e5a803f133b87f36f73ca5
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-07-08 09:58:21 +0300

    Solve without functions

--- leetcode/0150-evaluate-reverse-polish-notation.py
+++ leetcode/0150-evaluate-reverse-polish-notation.py
@@ -28,7 +28,27 @@ class Solution:
         return operands[0]
 
 
+class Solution2:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operands = []
+        for t in tokens:
+            if t == "+":
+                operands.append(operands.pop() + operands.pop())
+            elif t == "-":
+                a, b = operands.pop(), operands.pop()
+                operands.append(b - a)
+            elif t == "*":
+                operands.append(operands.pop() * operands.pop())
+            elif t == "/":
+                a, b = operands.pop(), operands.pop()
+                operands.append(int(b / a))
+            else:
+                operands.append(int(t))
+
+        return operands[0]
+
+
 tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
 
-solution = Solution()
+solution = Solution2()
 print(solution.evalRPN(tokens))
